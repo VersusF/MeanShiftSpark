98ee553cc9248be65d59611c44406e1e4b804d0f
Filippo Contro <controfilippo8@gmail.com>
2019-04-16 16:20:19 +0200

    Added dataset. Clustering on small one works

--- meanShift.py
+++ meanShift.py
@@ -3,7 +3,8 @@ from numpy import exp
 from pyspark import SparkContext
 
 sc = SparkContext("local", "first app")
-input_file = "dataset_small.csv"
+sc.setLogLevel("ERROR")
+input_file = "dataset.csv"
 MAX_X = 100
 MAX_Y = 100
 DISTANCE_LIMIT = 3
@@ -24,8 +25,11 @@ def gaussian_kernel(distance, bandwidth):
     return val
 
 
-def getChunk(point):
-    x, y = point[0], point[1]
+def getChunk(point, y=None):
+    if y is None:
+        x, y = point[0], point[1]
+    else:
+        x = point
     return int(x/CHUNK_SIZE), int(y/CHUNK_SIZE)
 
 
@@ -53,6 +57,10 @@ def generate_chunks(point):
     return result
 
 
+######################################
+#       MAP - REDUCE FUNCTIONS       #
+######################################
+
 def mapper(chunk):
     chunk_id, points = chunk[0], chunk[1]
     result = []
@@ -73,13 +81,53 @@ def mapper(chunk):
             # do the shift
             new_x /= total_weight
             new_y /= total_weight
-            result.append((new_x, new_y))
-    return chunk_id, result
+            # new_point = (getChunk(new_x, new_y), (new_x, new_y))
+            new_chunks = generate_chunks((new_x, new_y))
+            result.extend(new_chunks)
+    return result
 
 
-def merger(points):
-    # TODO merge points nearer than EPSILON
+# def merger(points, x):
+#     # merge points nearer than EPSILON
+#     # print 'merging', x, 'with', points
+#     for y in points:
+#         if distance(x, y) <= EPSILON:
+#             return points
+#     points.add(x)
+#     return points
+
+
+def combiner(set_a, set_b):
+    # combines two sets merging
+    # print 'combining', set_b, 'with', set_a
+    for x in set_b:
+        should_add = True
+        for y in set_a:
+            # if two points are near do not add
+            if distance(x, y) <= EPSILON:
+                should_add = False
+                break
+        if should_add:
+            set_a.add(x)
+    return set_a
+
+
+def unwrap(chunked_centroids):
+    chunk_id, centroids = chunked_centroids[0], chunked_centroids[1]
+    result = []
+    for centroid in centroids:
+        if getChunk(centroid) == chunk_id:
+            result.append(centroid)
+    return result
+
+
+def grouper(points, new_point):
+    points.add(new_point)
     return points
+
+
+def toString(point):
+    return str(point[0]) + ',' + str(point[1])
 
 
 def main():
@@ -89,23 +137,21 @@ def main():
     points = points.map(lambda my_line: (my_line[0], float(my_line[1]),
                                          float(my_line[2])))
     coords = points.map(lambda x: (x[1], x[2]))
+
     # Generate chunks where every points is associated to at most 9 chunks
-    # chunks = coords.map(lambda point: (getChunk(point[0], point[1]),) +
-    #                                    point)
-    chunks = coords.flatMap(generate_chunks).groupByKey()
+    chunks = coords.flatMap(generate_chunks).foldByKey(set(), grouper)
 
+    # MAP
+    new_points = chunks
     for i in range(ITERATION_NUMBER):
-        new_points = chunks.flatMap(mapper)
+        new_points = new_points.flatMap(mapper).foldByKey(set(), grouper)
 
-        print '\n\n\n\n'
-        print new_points.collect()
-        print '\n\n\n'
-
-        new_points = chunks.map(merger)
-
-        print '\n\n\n\n'
-        print new_points.collect()
-        print '\n\n\n'
+    # REDUCE
+    # centroids = new_points.aggregateByKey(set(), merger, combiner)
+    print '\n\n\n\t\t\tREDUCING\n\n\n'
+    centroids = new_points.foldByKey(set(), combiner)
+    centroids = centroids.flatMap(unwrap)
+    centroids.map(toString).saveAsTextFile('output')
 
 
 if __name__ == "__main__":
